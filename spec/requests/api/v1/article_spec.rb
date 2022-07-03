ce9e0fea6e4d6471b7d0a2c4e4721ee3c3703a74
shinohara <>
2022-07-03 16:52:47 +0900

    Task.7-3 記事一覧の実装(修正)

--- spec/requests/api/v1/article_spec.rb
+++ spec/requests/api/v1/article_spec.rb
@@ -1,8 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /index" do
     subject { get(api_v1_articles_path) }
+
     let!(:article1) { create(:article, updated_at: 1.days.ago) }
     let!(:article2) { create(:article, updated_at: 2.days.ago) }
     let!(:article3) { create(:article, updated_at: 3.days.ago) }
@@ -12,7 +13,7 @@ RSpec.describe "Api::V1::Articles", type: :request do
 
       res = JSON.parse(response.body)
       expect(res.length).to eq 3
-      expect(res.map{|a| a["id"]}).to eq [article1.id, article2.id, article3.id]
+      expect(res.map {|a| a["id"] }).to eq [article1.id, article2.id, article3.id]
       expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
       expect(response).to have_http_status(200)
