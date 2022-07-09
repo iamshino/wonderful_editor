084fdb0474ab12522757147cab6a1831e609afb4
shinohara <>
2022-07-09 13:43:11 +0900

    Task.7-4 記事詳細の実装(修正)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 3.times {
   User.create!(
     name: Faker::Name.name,
